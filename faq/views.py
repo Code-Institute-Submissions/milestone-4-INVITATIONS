9015152d7a1a70af974f6b0b11e6c66a975a353b
devtoguk <devtoguk@gmail.com>
2021-01-28 21:35:33 +0000

    Create template and view for the faq

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Faq
+
+
+def display_faq(request):
+    """ A view to show all the FAQs in display order, lowest first """
+
+    faqs = Faq.objects.all().order_by('display')
+    print(faqs)
+    context = {
+        'faqs': faqs,
+    }
+
+    return render(request, 'faq/faq.html', context)
