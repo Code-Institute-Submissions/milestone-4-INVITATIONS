2a02d1d5b00670af4acc9640044a9529c2a90b83
devtoguk <devtoguk@gmail.com>
2021-01-28 20:27:38 +0000

    Get secret key from environment variable

--- invitations/settings.py
+++ invitations/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-2c1y0af9o-ea%&pz5lk-8m9%8m@*h)ba*4wt*h65+1-+3@a&='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
