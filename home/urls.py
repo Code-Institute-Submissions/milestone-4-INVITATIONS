8e1d2966786b3030d8970634cc94d6fd50c7d47f
devtoguk <devtoguk@gmail.com>
2021-01-28 14:15:06 +0000

    Add views and templates for the other static pages
    Our story, Terms, Privacy and delivery information.

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('our_story/', views.our_story, name='our_story'),
+    path('terms_conditions/', views.terms_conditions, name='terms_conditions'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
+    path('delivery_info/', views.delivery_info, name='delivery_info'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render
 
 
 def index(request):
-    """ Index / ome page view """
+    """ Index / home page view """
 
     return render(request, 'home/index.html')
+
+
+def our_story(request):
+    """ Our Story page view """
+
+    return render(request, 'home/our-story.html')
+
+
+def terms_conditions(request):
+    """ Terms and Conditions page view """
+
+    return render(request, 'home/terms.html')
+
+
+def privacy_policy(request):
+    """ Privacy Policy page view """
+
+    return render(request, 'home/privacy.html')
+
+
+def delivery_info(request):
+    """ Delivery Information page view """
+
+    return render(request, 'home/delivery.html')
