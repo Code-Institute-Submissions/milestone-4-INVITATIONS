a092b17a0327e47970c71daa7e1708bcdb38d8e5
devtoguk <devtoguk@gmail.com>
2021-02-04 22:11:04 +0000

    Create update cart functionality
    Take any changes to the shopping cart quantities and apply them to the
    shopping cart session variable.

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<product_id>/', views.remove_item, name='remove_item'),
+    path('update/', views.update_cart_qty, name='update_cart_qty'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -24,6 +24,7 @@ def add_to_cart(request, product_id):
         print(f'New Prod count: {quantity}')
         print(f'Keys: {list(cart.keys())}')
 
+    print(f'Cart: {cart}')
     request.session['cart'] = cart
     return redirect(original_path)
 
@@ -36,6 +37,25 @@ def remove_item(request, product_id):
     print(type(cart))
     cart.pop(product_id)
     print(cart)
+
+    request.session['cart'] = cart
+    return render(request, 'cart/cart.html')
+
+
+def update_cart_qty(request):
+    # form_data = request.POST.items()
+    form_data = list(request.POST.items())
+    print(f'Form data: {form_data}')
+    print(type(form_data))
+    cart = {}
+
+    for product_id_element, item_quantity in form_data:
+        print(f'Part= {product_id_element[0:3]}')
+        if product_id_element[0:3] == 'qty':
+            product_id = product_id_element.split('-')[-1]
+            item_quantity = int(item_quantity)
+            print(f'Product ID: {product_id_element} is ID: {product_id} with qty of: {item_quantity}')
+            cart[product_id] = item_quantity
 
     request.session['cart'] = cart
     return render(request, 'cart/cart.html')
