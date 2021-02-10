47c647a850a8ff5eccf2267b9a112187294c964b
devtoguk <devtoguk@gmail.com>
2021-02-10 11:05:45 +0000

    Update the checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_date',
+                       'order_total', 'delivery_cost',
+                       'grand_total', 'original_cart',
+                       'stripe_pid')
+
+    fields = ('order_date', 'full_name',
+              'email', 'phone_number', 'street_address1',
+              'street_address2', 'postcode', 'town_or_city',
+              'county', 'country', 'order_total',
+              'delivery_cost', 'grand_total', 'original_cart',
+              'stripe_pid')
+
+    list_display = ('pk', 'order_date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
+
+    ordering = ('-order_date',)
+
+
+admin.site.register(Order, OrderAdmin)
