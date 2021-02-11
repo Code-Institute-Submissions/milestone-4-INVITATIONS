45beb5b39534f5ee1e0aeea5144ca1bea7aff899
devtoguk <devtoguk@gmail.com>
2021-02-11 09:57:42 +0000

    Finish styling crispy form layout for current fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Order
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column, HTML
+from crispy_forms.layout import Layout, Row, Column, HTML, Field
 
 
 class OrderForm(forms.ModelForm):
@@ -26,24 +26,34 @@ class OrderForm(forms.ModelForm):
         self.helper.field_class = 'pb-0 mb-0'
         self.helper.form_method = 'post'
         self.helper.form_show_labels = True
-        self.helper.form_action = 'submit_survey'
-        self.helper.layout = Layout('full_name',
-                                    'email',
-                                    HTML('<p class="mt-3 mb-1">Delivery Details:</p>'),
-                                    'street_address1',
-                                    'street_address2',
-                                    Row(
-                                        Column('town_or_city', css_class='form-group col-sm-6 mb-0'),
-                                        Column('county', css_class='form-group col-sm-6 mb-0 pl-0 pr-1'),
-                                        css_class='form-row'
-                                    ),
-                                    Row(
-                                        Column('postcode', css_class='form-group col-sm-6 mb-0'),
-                                        Column('country', css_class='form-group col-sm-6 mb-0 pl-0 pr-1'),
-                                        css_class='form-row'
-                                    ),)
+        self.helper.form_action = '/checkout/'
+        self.helper.layout = Layout(
+            HTML('<p class="small text-muted mb-1">Your Details</p>'),
+            Field('full_name', css_class="rounded-0"),
+            Field('email', css_class="rounded-0"),
+            HTML('<p class="small text-muted mt-3 mb-1">Delivery Details</p>'),
+            Field('street_address1', css_class="rounded-0"),
+            Field('street_address2', css_class="rounded-0"),
+            Row(
+                Column(Field('town_or_city', css_class="rounded-0"),
+                       css_class='form-group col-sm-6 mb-0'),
+                Column(Field('county', css_class="rounded-0"),
+                       css_class='form-group col-sm-6 mb-0 pl-0 pr-1'),
+                css_class='form-row'),
+            Row(
+                Column(Field('postcode', css_class="rounded-0"),
+                       css_class='form-group col-sm-6 mb-0'),
+                Column(Field('country', css_class="rounded-0"),
+                       css_class='form-group col-sm-6 mb-0 pl-0 pr-1'),
+                css_class='form-row'
+            ),
+            HTML('<button type="submit" '
+                 'class="btn btn-primary rounded-0 mt-2 float-right">'
+                 '<i class="fas fa-lock icon" aria-hidden="true"></i>'
+                 'Make Payment</button>'),
+        )
 
-        self.helper.add_input(Submit('submit', 'Submit'))
+        # self.helper.add_input(Submit('submit', 'Make Payment'))
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
 
