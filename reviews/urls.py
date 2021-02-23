4fa31a82cb3272af106f55bd0ae134d8b5e23dc4
devtoguk <devtoguk@gmail.com>
2021-02-23 17:15:47 +0000

    Allow user to add a product review

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.reviews, name='reviews'),
+    path('add/<int:product_id>/<int:order_id>/', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ReviewForm
+from products.models import Product
+
+from django.contrib import messages
 
 
 def reviews(request):
-    """ A view to show your reviews """
+    """ A view add a new review """
 
     return render(request, 'reviews/reviews.html')
+
+
+def add_review(request, product_id, order_id):
+    """ A view add a new review """
+    print('Reviewing product: ', product_id)
+
+    if request.POST:
+        form_data = {
+            'comment': request.POST['comment'],
+            'rating': request.POST['rating'],
+            'product': product_id,
+            'user': request.user.id,
+        }
+
+        form = ReviewForm(form_data)
+
+        if form.is_valid():
+            print('Form was valid')
+            form.save()
+            messages.success(request, 'Your review has been successfully \
+                             added',
+                             extra_tags='reviews')
+            return redirect('home')
+
+        else:
+            print('Form FAILED')
+            messages.error(request, 'Review submission failed,  please \
+                           double-check your form and retry.',
+                           extra_tags='reviews')
+
+    else:
+        # form = ReviewForm(initial={'product': product_id})
+        form = ReviewForm()
+
+    form.helper.form_action += f'/{product_id}/{order_id}/'
+    context = {
+        'form': form,
+    }
+    return render(request, 'reviews/add_review.html', context)
