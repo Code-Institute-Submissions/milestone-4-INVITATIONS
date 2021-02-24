85cf7207f702783417368259972b760f1389faef
devtoguk <devtoguk@gmail.com>
2021-02-24 20:57:45 +0000

    Tidy comments & remove any prtint statements

--- reviews/views.py
+++ reviews/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .forms import ReviewForm
 from products.models import Product, ProductReviews
-from checkout.models import Order, OrderLineItem
-
-from django.contrib import messages
+from checkout.models import OrderLineItem
 
 
-# def reviews(request):
-#     """ A view add a new review """
-
-#     return render(request, 'reviews/reviews.html')
 def add_review(request, product_id, order_id):
     """ A view to add a new review """
-    print('Reviewing product: ', product_id)
-
     try:
         order_lines = OrderLineItem.objects.get(order=int(order_id),
                                                 product=int(product_id))
@@ -23,13 +16,12 @@ def add_review(request, product_id, order_id):
                        extra_tags='reviews')
         return redirect('user_profile')
 
-    print('Order: ', order_lines)
     ordered_by = order_lines.order
-    # print('User', ordered_by.first_name)
 
     if request.user != ordered_by.user:
-        messages.error(request, 'Order not found in your order history with that product line.',
-                         extra_tags='reviews')
+        messages.error(request, 'Order not found in your order history with \
+                                that product line.',
+                                extra_tags='reviews')
         return redirect('user_profile')
 
     if request.POST:
@@ -43,7 +35,6 @@ def add_review(request, product_id, order_id):
         form = ReviewForm(form_data)
 
         if form.is_valid():
-            print('Form was valid')
             form.save()
             messages.success(request, 'Your review has been successfully \
                              added',
@@ -51,7 +42,6 @@ def add_review(request, product_id, order_id):
             return redirect('home')
 
         else:
-            print('Form FAILED')
             messages.error(request, 'Review submission failed,  please \
                            double-check your form and retry.',
                            extra_tags='reviews')
@@ -60,7 +50,6 @@ def add_review(request, product_id, order_id):
         form = ReviewForm(is_add=True)
 
     form.helper.form_action += f'add/{product_id}/{order_id}/'
-
     product = Product.objects.get(pk=product_id)
 
     context = {
@@ -73,18 +62,16 @@ def add_review(request, product_id, order_id):
 
 def edit_review(request, review_id):
     """ A view to edit/delete reviews reviews """
-    print('Editing review: ', review_id)
-
     try:
         review = ProductReviews.objects.get(pk=int(review_id))
     except ProductReviews.DoesNotExist:
         messages.error(request, 'Review has not been found.',
-                         extra_tags='reviews')
+                       extra_tags='reviews')
         return redirect('user_profile')
 
     if request.user != review.user:
         messages.error(request, 'Review not found in your profile',
-                         extra_tags='reviews')
+                       extra_tags='reviews')
         return redirect('user_profile')
 
     product = Product.objects.get(pk=review.product.pk)
@@ -92,9 +79,7 @@ def edit_review(request, review_id):
     if request.POST:
         try:
             delete_or_not = int(request.POST['delete-review'])
-            print(f'Delete checkbox is set to: {delete_or_not}')
         except KeyError:
-            print('Delete box NOT checked')
             delete_or_not = 0
 
         form_data = {
@@ -108,10 +93,8 @@ def edit_review(request, review_id):
         form = ReviewForm(form_data, instance=review)
 
         if form.is_valid():
-            print('Form was valid')
 
             if delete_or_not == 1:
-                print('Delete box IS checked')
                 review.delete()
                 messages.success(request, 'Your review has been deleted',
                                  extra_tags='reviews')
@@ -124,7 +107,6 @@ def edit_review(request, review_id):
                 return redirect('user_profile')
 
         else:
-            print('Form FAILED')
             messages.error(request, 'Review edit failed,  please \
                            double-check your form and retry.',
                            extra_tags='reviews')
@@ -133,11 +115,6 @@ def edit_review(request, review_id):
         form = ReviewForm(instance=review)
 
     form.helper.form_action += f'edit/{review_id}/'
-
-    # print('Layout', list(form.helper.layout))
-    # print('Layout', form.helper.layout[3])
-    # print('Layout', form.helper.layout[3].html)
-    # print('Layout', type(form.helper.layout[3]))
 
     context = {
         'form': form,
