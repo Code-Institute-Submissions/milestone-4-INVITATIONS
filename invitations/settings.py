d262c8ac8797a6e7fd0df63e170e8b67ffc36488
devtoguk <devtoguk@gmail.com>
2021-02-25 15:57:51 +0000

    Update database settings for deployment

--- invitations/settings.py
+++ invitations/settings.py
@@ -15,6 +15,8 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,13 +128,18 @@ WSGI_APPLICATION = 'invitations.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
